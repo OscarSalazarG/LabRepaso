722afea6d04b0d0c13a907921aabcccfb3c08b0e
Oscar Eduardo Salazar García <edu082000@hotmail.com>
2023-10-14 00:18:17 -0500

    fue

--- app/src/main/java/com/example/labrepaso/MainActivity.kt
+++ app/src/main/java/com/example/labrepaso/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.labrepaso
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnLog: Button = findViewById(R.id.btnLogin)
 
+        btnLog.setOnClickListener{
+            this.login();
+        }
+
+
+    }
+    private fun login()
+    {
+        val intent = Intent(this,MenuActivity::class.java)
+        startActivity(intent)
     }
 }
